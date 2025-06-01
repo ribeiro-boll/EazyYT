2466345fd52f3b776a8e7e1800db06629a3e588e
bolota <luccajetx@gmail.com>
2025-06-01 03:20:33 -0300

    limpar repositorio

--- hooks/hook-yt_dlp.py
+++
@@ -1,5 +0,0 @@
-# hooks/hook-yt_dlp.py
-from PyInstaller.utils.hooks import collect_submodules
-
-# collect *all* yt_dlp submodules (incl. compat & utils._deprecated)
-hiddenimports = collect_submodules('yt_dlp')
